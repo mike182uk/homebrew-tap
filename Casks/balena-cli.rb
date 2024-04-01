4f356e4769cd7c3b96170e60f25ed0c6cd3223d3
Michael Barrett <mike182uk@gmail.com>
2024-04-01 18:02:42 +0100

    upgrade balena-cli

--- Casks/balena-cli.rb
+++ Casks/balena-cli.rb
@@ -1,10 +1,10 @@
 cask "balena-cli" do
-  version "17.1.1"
-  sha256 "abb8c6143fa85c6fddb431dd6d8e66562af9ae7f3d61d7737eb0f738e629b8ff"
+  version "18.1.5"
+  sha256 "3de7bc0c22d3022cf082819a59e8072d883b04d30f65deb607eeff0bca6b8ed2"
 
   url "https://github.com/balena-io/balena-cli/releases/download/v#{version}/balena-cli-v#{version}-macOS-x64-installer.pkg"
-  name "Balena CLI"
-  desc "Official Balena CLI tool"
+  name "balena CLI"
+  desc "Official balena command-line interface"
   homepage "https://github.com/balena-io/balena-cli"
 
   livecheck do
@@ -15,4 +15,6 @@ cask "balena-cli" do
   pkg "balena-cli-v#{version}-macOS-x64-installer.pkg"
 
   uninstall pkgutil: "io.balena.cli"
+
+  zap trash: "~/.balena"
 end
